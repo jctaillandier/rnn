cfaa863e5d88f171e94554b04c48e27a1b7ebebc
J-C <jc.taillandier@hotmail.com>
2019-03-07 11:57:43 -0500

    figuring out tensors sizes for fprop

--- models.py
+++ models.py
@@ -154,8 +154,8 @@ class RNN(nn.Module): # Implement a stacked vanilla RNN with Tanh nonlinearities
                 # Here I work on each layers with 
                 # pre activation:
                 # row index 0 is the firt row
-                hidden = self.rec_layers[layer](hidden_states[layer, timestep]
-                y = self.regular_layers[layer](x) + hidden)
+                hidden = self.rec_layers[layer](hidden_states[layer, timestep])
+                y = (self.regular_layers[layer](x) + hidden)
                 # layer output
                 x = torch.tanh(y)
                 print('size of thing to store in hidden_states: ', x.shape)
