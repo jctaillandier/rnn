bed4b73172faf342b81df9df4ff87eefeb72e9f0
J-C <jc.taillandier@hotmail.com>
2019-03-08 14:47:24 -0500

    First reg layer is differnet size to adjust embedding to hidden_size

--- models.py
+++ models.py
@@ -187,13 +187,13 @@ class RNN(nn.Module): # Implement a stacked vanilla RNN with Tanh nonlinearities
             ## AJOUTER LINEAR LAYER SANS ACTIVATION, DROPOUT
             z = self.decoder(x)
             z = z.to(device)
-            #print('after decoded at each layer: ', z.shape)
-            #print()
+            print('after decoded at each layer: ', z.shape)
+            print()
             
             logits[0,:,:] = z[self.num_layers-1,:,:]
             logits = logits.to(device)
             
-        #print('logits final size: ', logits.shape)
+            print('logits size: ', logits.shape)
         
         """
         
